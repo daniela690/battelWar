442a2dbb0a80b7858cc1992bfc9de5442c877085
danie <danie@>
2025-09-12 12:34:55 +0300

    Add project files.

---
+++ battelWar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace battelWar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
